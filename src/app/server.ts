af2d5e077805edec6bf681a5a058daa42d00babc
Vectoras <johnny.boghean.developer@gmail.com>
2024-09-10 08:53:30 +0100

    previously not commited changes

--- src/app/server.ts
+++ src/app/server.ts
@@ -2,6 +2,7 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import passport from "passport";
 // importing custom middleware
 import { addRequestFeedbackMiddleware } from "./middleware/response_format/add_request_feedback.js";
 // importing routers
@@ -29,6 +30,7 @@ app.use(
 );
 app.use(express.json());
 app.use(addRequestFeedbackMiddleware);
+app.use(passport.initialize());
 
 // routers
 
